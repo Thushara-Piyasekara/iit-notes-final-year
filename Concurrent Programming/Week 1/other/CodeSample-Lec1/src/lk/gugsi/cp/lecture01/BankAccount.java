a780b78a1e73acccf5ce1c7d6119d6fa1fb15992
Thushara-Piyasekara <btgp2001@gmail.com>
2024-10-12 10:18:12 +0530

    vault backup: 2024-10-12 10:18:12

--- Concurrent Programming/Week 1/other/CodeSample-Lec1/src/lk/gugsi/cp/lecture01/BankAccount.java
+++ Concurrent Programming/Week 1/other/CodeSample-Lec1/src/lk/gugsi/cp/lecture01/BankAccount.java
@@ -16,7 +16,7 @@ public class BankAccount {
 	// balance variable must happen inside synchronized 
 	// block
 	
-	pblic BankAccount(double balance, String accountNo) {
+	public BankAccount(double balance, String accountNo) {
 		super();
 		this.balance = balance;
 		this.accountNo = accountNo;
